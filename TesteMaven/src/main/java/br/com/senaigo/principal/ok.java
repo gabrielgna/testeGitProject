0262e584c8c30ae3d250cfbb1a0a1b2bea426171
443384 <443384@CELGCLG080410.celg.go>
2019-09-04 22:09:39 -0300

    Initial Commit for Maven Project

---
+++ TesteMaven/src/main/java/br/com/senaigo/principal/ok.java
@@ -0,0 +1,12 @@
+package br.com.senaigo.principal;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ok {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(StringUtils.capitalize("teste"));
+	}
+
+}
